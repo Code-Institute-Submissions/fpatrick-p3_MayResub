1d00f096a2348b0bf80db69f81299851ec3cc338
erick <fpatrick.erick@gmail.com>
2022-04-12 15:45:44 +0100

    Add class choice to get and validate input for menu

--- run.py
+++ run.py
@@ -1,1 +1,43 @@
-print('Hello world')
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://realpython.github.io/fake-jobs/"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find(id="ResultsContainer")
+print(results.prettify())
+
+
+class Choice:
+    """
+    Instance the user choice
+    :param choice: The menu choice, can be a number 1-6
+    """
+
+    def __init__(self, choice):
+        self.choice = choice
+
+    def validate_choice(self):
+        """
+        Convert choice to int, raise exception if wrong option or can't be converted
+        :return: True if passes, False if except
+        """
+        try:
+            if int(self.choice) > 6:
+                raise ValueError(
+                    f"Please choose a number between 0 and 6, you provided {self.choice}"
+                )
+        except ValueError as e:
+            print(f"Invalid data: {e}, please try again. \n")
+            return False
+        return True
+
+
+while True:
+    print("Welcome to . \n")
+    number = input("Enter bla bla: \n")
+    choice = Choice(number)
+    if choice.validate_choice():
+        print("Valid")
+        break
