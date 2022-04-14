7e8bab15037e51c2ef1e18d941baa1992324e049
erick <fpatrick.erick@gmail.com>
2022-04-14 12:05:26 +0100

    Add method to ask and validate price

--- action.py
+++ action.py
@@ -1,32 +1,45 @@
-class Choice:
+class Validate:
     """
     Get menu choice from user and validate
     :param option: The menu choice, can be a number 1-6
     """
-    def __init__(self, option):
-        self.option = option
+    def __init__(self):
+        self.choice = ""
+        self.number = 0
 
-    def validate_choice(self):
+    def validate_choice(self, choice):
         """
         Convert choice to int, raise exception if wrong option or can't be converted
         :return: True if passes, False if except
         """
         try:
-            if int(self.option) > 6:
+            if int(choice) > 6:
                 raise ValueError(
-                    f"Please choose a number between 0 and 6! You provided {self.option}"
+                    f"Please choose a number between 0 and 6! You provided {choice}"
                 )
         except ValueError as e:
             print(f"Invalid data: {e}, please try again. \n")
             return False
-        self.option = int(self.option)
+        self.choice = int(choice)
         return True
+
+    def ask_price(self):
+        while True:
+            try:
+                self.number = int(input("Please enter desired price: \n"))
+                return False
+            except ValueError as e:
+                print(f"Invalid data! Please enter only numbers and try again. \n")
+
 
 
 class User:
-    def __init__(self, **kwargs):
-        self.amazon_url = kwargs["amazon"]
-        self.argos_url = kwargs["argos"]
-        self.currys_url = kwargs["currys"]
+    def __init__(self):
+        self.amazon_url = ""
+        self.amazon_price = 0
+        self.argos_url = ""
+        self.argos_price = 0
+        self.currys_url = ""
+        self.currys_price = 0
 
 
